3854893786ddedb56e7ebce0c2a82a176e72eda4
Mike <mike.huang.mikank@gmail.com>
2021-05-24 16:54:27 +0800

    Moved directory and shop data into own reducer, and created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
+import { Route } from 'react-router-dom';
 
-  render () {
-    return (
-      <div className='shop-page'>
-        {
-          this.state.collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    )
-  }
-}
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,30 @@
 // root-reducer represent all of the state of the app
-
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+// use localStorage as the default storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+// configurations for Redux to use
+const persistConfig = {
+  // at that point inside of reducer obj to start storing
+  key: 'root',
+  storage,
+  // containing the names of all the reducers we want to persist
+  whiteList: ['cart']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
-
+// modified version of the root reducer with persistence capabilities
+export default persistReducer(persistConfig, rootReducer);

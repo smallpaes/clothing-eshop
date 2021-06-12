73866107d3d44cf21cdfe46a456b81d97243b0ab
Mike <mike.huang.mikank@gmail.com>
2021-06-12 15:47:06 +0800

    Update styles for mobile screen

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPageContainer from './pages/shop/shop.container';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPageContainer from './pages/checkout/checkout.container';
-
 import HeaderContainer from './components/header/header.container';
+
+import { GlobalStyle } from './global.styles';
 
 import { selectCurrentUser  } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
@@ -22,6 +22,8 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      {/* Apply all styles to the application */}
+      <GlobalStyle />
       <HeaderContainer />
       <Switch>
         <Route exact path='/' component={HomePage} />
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -7,6 +7,12 @@ export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+
+  @media screen and (max-width: 800px) {
+    height: 60px;
+    padding: 10px;
+    margin-bottom: 20px;
+  }
 `;
 
 // calling styled as a function to pass in custom component
@@ -15,6 +21,11 @@ export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+
+  @media screen and (max-width: 800px) {
+    width: 50px;
+    padding: 0;
+  }
 `;
 
 export const OptionsContainer = styled.div`
@@ -23,6 +34,10 @@ export const OptionsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
 `;
 
 export const OptionLink = styled(Link)`
